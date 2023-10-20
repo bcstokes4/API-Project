98d157a36e0520096516bceed47de3e01b11ebde
Brian Stokes <bcstokes4@gmail.com>
2023-10-20 12:19:28 -0400

    added signup component/redux thunk and actions to handle request to server

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,7 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+//ACTION CREATORS
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -16,6 +17,7 @@ const removeUser = () => {
   };
 };
 
+//THUNKS
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -36,6 +38,24 @@ export const restoreUser = () => async (dispatch) => {
     dispatch(setUser(data.user));
     return response;
   };
+
+  export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 
 const initialState = { user: null };
 
